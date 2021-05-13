9f3b10d0d3d582c539086ce29a343de5ececc8e0
colin <colins110@hotmail.com>
2021-05-13 15:01:55 +0800

    add some special setting for rocksdb

--- core/core_workload.cc
+++ core/core_workload.cc
@@ -6,17 +6,20 @@
 //  Copyright (c) 2014 Jinglei Ren <jinglei@ren.systems>.
 //
 
-#include "uniform_generator.h"
-#include "zipfian_generator.h"
+#include "core_workload.h"
+#include "const_generator.h"
 #include "scrambled_zipfian_generator.h"
 #include "skewed_latest_generator.h"
-#include "const_generator.h"
-#include "core_workload.h"
+#include "uniform_generator.h"
+#include "zipfian_generator.h"
 
 #include <string>
 
-using ycsbc::CoreWorkload;
 using std::string;
+using ycsbc::CoreWorkload;
+
+const string CoreWorkload::DBNAME_PROPERTY = "dbname";
+const string CoreWorkload::DBNAME_DEFAULT = "basic";
 
 const string CoreWorkload::TABLENAME_PROPERTY = "table";
 const string CoreWorkload::TABLENAME_DEFAULT = "usertable";
@@ -77,47 +80,48 @@ const string CoreWorkload::RECORD_COUNT_PROPERTY = "recordcount";
 const string CoreWorkload::OPERATION_COUNT_PROPERTY = "operationcount";
 
 void CoreWorkload::Init(const utils::Properties &p) {
-  table_name_ = p.GetProperty(TABLENAME_PROPERTY,TABLENAME_DEFAULT);
-  
-  field_count_ = std::stoi(p.GetProperty(FIELD_COUNT_PROPERTY,
-                                         FIELD_COUNT_DEFAULT));
+  table_name_ = p.GetProperty(TABLENAME_PROPERTY, TABLENAME_DEFAULT);
+
+  field_count_ =
+      std::stoi(p.GetProperty(FIELD_COUNT_PROPERTY, FIELD_COUNT_DEFAULT));
   field_len_generator_ = GetFieldLenGenerator(p);
-  
-  double read_proportion = std::stod(p.GetProperty(READ_PROPORTION_PROPERTY,
-                                                   READ_PROPORTION_DEFAULT));
-  double update_proportion = std::stod(p.GetProperty(UPDATE_PROPORTION_PROPERTY,
-                                                     UPDATE_PROPORTION_DEFAULT));
-  double insert_proportion = std::stod(p.GetProperty(INSERT_PROPORTION_PROPERTY,
-                                                     INSERT_PROPORTION_DEFAULT));
-  double scan_proportion = std::stod(p.GetProperty(SCAN_PROPORTION_PROPERTY,
-                                                   SCAN_PROPORTION_DEFAULT));
+
+  double read_proportion = std::stod(
+      p.GetProperty(READ_PROPORTION_PROPERTY, READ_PROPORTION_DEFAULT));
+  double update_proportion = std::stod(
+      p.GetProperty(UPDATE_PROPORTION_PROPERTY, UPDATE_PROPORTION_DEFAULT));
+  double insert_proportion = std::stod(
+      p.GetProperty(INSERT_PROPORTION_PROPERTY, INSERT_PROPORTION_DEFAULT));
+  double scan_proportion = std::stod(
+      p.GetProperty(SCAN_PROPORTION_PROPERTY, SCAN_PROPORTION_DEFAULT));
   double readmodifywrite_proportion = std::stod(p.GetProperty(
       READMODIFYWRITE_PROPORTION_PROPERTY, READMODIFYWRITE_PROPORTION_DEFAULT));
-  
+
   record_count_ = std::stoi(p.GetProperty(RECORD_COUNT_PROPERTY));
   std::string request_dist = p.GetProperty(REQUEST_DISTRIBUTION_PROPERTY,
                                            REQUEST_DISTRIBUTION_DEFAULT);
-  zero_padding_ = std::stoi(p.GetProperty(ZERO_PADDING_PROPERTY, ZERO_PADDING_DEFAULT));
-  int max_scan_len = std::stoi(p.GetProperty(MAX_SCAN_LENGTH_PROPERTY,
-                                             MAX_SCAN_LENGTH_DEFAULT));
+  zero_padding_ =
+      std::stoi(p.GetProperty(ZERO_PADDING_PROPERTY, ZERO_PADDING_DEFAULT));
+  int max_scan_len = std::stoi(
+      p.GetProperty(MAX_SCAN_LENGTH_PROPERTY, MAX_SCAN_LENGTH_DEFAULT));
   std::string scan_len_dist = p.GetProperty(SCAN_LENGTH_DISTRIBUTION_PROPERTY,
                                             SCAN_LENGTH_DISTRIBUTION_DEFAULT);
-  int insert_start = std::stoi(p.GetProperty(INSERT_START_PROPERTY,
-                                             INSERT_START_DEFAULT));
-  
-  read_all_fields_ = utils::StrToBool(p.GetProperty(READ_ALL_FIELDS_PROPERTY,
-                                                    READ_ALL_FIELDS_DEFAULT));
-  write_all_fields_ = utils::StrToBool(p.GetProperty(WRITE_ALL_FIELDS_PROPERTY,
-                                                     WRITE_ALL_FIELDS_DEFAULT));
-  
+  int insert_start =
+      std::stoi(p.GetProperty(INSERT_START_PROPERTY, INSERT_START_DEFAULT));
+
+  read_all_fields_ = utils::StrToBool(
+      p.GetProperty(READ_ALL_FIELDS_PROPERTY, READ_ALL_FIELDS_DEFAULT));
+  write_all_fields_ = utils::StrToBool(
+      p.GetProperty(WRITE_ALL_FIELDS_PROPERTY, WRITE_ALL_FIELDS_DEFAULT));
+
   if (p.GetProperty(INSERT_ORDER_PROPERTY, INSERT_ORDER_DEFAULT) == "hashed") {
     ordered_inserts_ = false;
   } else {
     ordered_inserts_ = true;
   }
-  
+
   key_generator_ = new CounterGenerator(insert_start);
-  
+
   if (read_proportion > 0) {
     op_chooser_.AddValue(READ, read_proportion);
   }
@@ -133,12 +137,12 @@ void CoreWorkload::Init(const utils::Properties &p) {
   if (readmodifywrite_proportion > 0) {
     op_chooser_.AddValue(READMODIFYWRITE, readmodifywrite_proportion);
   }
-  
+
   insert_key_sequence_.Set(record_count_);
-  
+
   if (request_dist == "uniform") {
     key_chooser_ = new UniformGenerator(0, record_count_ - 1);
-    
+
   } else if (request_dist == "zipfian") {
     // If the number of keys changes, we don't want to change popular keys.
     // So we construct the scrambled zipfian generator with a keyspace
@@ -148,41 +152,44 @@ void CoreWorkload::Init(const utils::Properties &p) {
     int op_count = std::stoi(p.GetProperty(OPERATION_COUNT_PROPERTY));
     int new_keys = (int)(op_count * insert_proportion * 2); // a fudge factor
     key_chooser_ = new ScrambledZipfianGenerator(record_count_ + new_keys);
-    
+
   } else if (request_dist == "latest") {
     key_chooser_ = new SkewedLatestGenerator(insert_key_sequence_);
-    
+
   } else {
     throw utils::Exception("Unknown request distribution: " + request_dist);
   }
-  
+
   field_chooser_ = new UniformGenerator(0, field_count_ - 1);
-  
+
   if (scan_len_dist == "uniform") {
     scan_len_chooser_ = new UniformGenerator(1, max_scan_len);
   } else if (scan_len_dist == "zipfian") {
     scan_len_chooser_ = new ZipfianGenerator(1, max_scan_len);
   } else {
     throw utils::Exception("Distribution not allowed for scan length: " +
-        scan_len_dist);
+                           scan_len_dist);
+  }
+  if (p.GetProperty(DBNAME_PROPERTY) == "rocksdb") {
+    handleRocksdb();
   }
 }
 
-ycsbc::Generator<uint64_t> *CoreWorkload::GetFieldLenGenerator(
-    const utils::Properties &p) {
+ycsbc::Generator<uint64_t> *
+CoreWorkload::GetFieldLenGenerator(const utils::Properties &p) {
   string field_len_dist = p.GetProperty(FIELD_LENGTH_DISTRIBUTION_PROPERTY,
                                         FIELD_LENGTH_DISTRIBUTION_DEFAULT);
-  int field_len = std::stoi(p.GetProperty(FIELD_LENGTH_PROPERTY,
-                                          FIELD_LENGTH_DEFAULT));
-  if(field_len_dist == "constant") {
+  int field_len =
+      std::stoi(p.GetProperty(FIELD_LENGTH_PROPERTY, FIELD_LENGTH_DEFAULT));
+  if (field_len_dist == "constant") {
     return new ConstGenerator(field_len);
-  } else if(field_len_dist == "uniform") {
+  } else if (field_len_dist == "uniform") {
     return new UniformGenerator(1, field_len);
-  } else if(field_len_dist == "zipfian") {
+  } else if (field_len_dist == "zipfian") {
     return new ZipfianGenerator(1, field_len);
   } else {
     throw utils::Exception("Unknown field length distribution: " +
-        field_len_dist);
+                           field_len_dist);
   }
 }
 
@@ -202,3 +209,20 @@ void CoreWorkload::BuildUpdate(std::vector<ycsbc::DB::KVPair> &update) {
   update.push_back(pair);
 }
 
+// rocksdb need special handle some property
+void CoreWorkload::handleRocksdb() {
+  // rocksdb only use one field
+  field_count_ = 1;
+
+  // reset the field_chooser_
+  if (field_chooser_ != nullptr) {
+    delete field_chooser_;
+  }
+  field_chooser_ = new UniformGenerator(0, field_count_ - 1);
+
+  // rocksdb always read all fields
+  read_all_fields_ = true;
+
+  // rocksdb always write all fields
+  write_all_fields_ = true;
+}
--- core/zipfian_generator.h
+++ core/zipfian_generator.h
@@ -9,42 +9,43 @@
 #ifndef YCSB_C_ZIPFIAN_GENERATOR_H_
 #define YCSB_C_ZIPFIAN_GENERATOR_H_
 
+#include "generator.h"
+#include "utils.h"
 #include <cassert>
 #include <cmath>
 #include <cstdint>
 #include <mutex>
-#include "utils.h"
 
 namespace ycsbc {
 
 class ZipfianGenerator : public Generator<uint64_t> {
- public:
+public:
   constexpr static const double kZipfianConst = 0.99;
   static const uint64_t kMaxNumItems = (UINT64_MAX >> 24);
-  
+
   ZipfianGenerator(uint64_t min, uint64_t max,
-                   double zipfian_const = kZipfianConst) :
-      num_items_(max - min + 1), base_(min), theta_(zipfian_const),
-      zeta_n_(0), n_for_zeta_(0) {
+                   double zipfian_const = kZipfianConst)
+      : num_items_(max - min + 1), base_(min), theta_(zipfian_const),
+        zeta_n_(0), n_for_zeta_(0) {
     assert(num_items_ >= 2 && num_items_ < kMaxNumItems);
     zeta_2_ = Zeta(2, theta_);
     alpha_ = 1.0 / (1.0 - theta_);
     RaiseZeta(num_items_);
     eta_ = Eta();
-    
+
     Next();
   }
-  
-  ZipfianGenerator(uint64_t num_items) :
-      ZipfianGenerator(0, num_items - 1, kZipfianConst) { }
-  
+
+  ZipfianGenerator(uint64_t num_items)
+      : ZipfianGenerator(0, num_items - 1, kZipfianConst) {}
+
   uint64_t Next(uint64_t num_items);
-  
+
   uint64_t Next() { return Next(num_items_); }
 
   uint64_t Last();
-  
- private:
+
+private:
   ///
   /// Compute the zeta constant needed for the distribution.
   /// Remember the number of items, so if it is changed, we can recompute zeta.
@@ -54,10 +55,10 @@ class ZipfianGenerator : public Generator<uint64_t> {
     zeta_n_ = Zeta(n_for_zeta_, num, theta_, zeta_n_);
     n_for_zeta_ = num;
   }
-  
+
   double Eta() {
     return (1 - std::pow(2.0 / num_items_, 1 - theta_)) /
-        (1 - zeta_2_ / zeta_n_);
+           (1 - zeta_2_ / zeta_n_);
   }
 
   ///
@@ -66,22 +67,22 @@ class ZipfianGenerator : public Generator<uint64_t> {
   /// Use the zipfian constant as theta. Remember the new number of items
   /// so that, if it is changed, we can recompute zeta.
   ///
-  static double Zeta(uint64_t last_num, uint64_t cur_num,
-                     double theta, double last_zeta) {
+  static double Zeta(uint64_t last_num, uint64_t cur_num, double theta,
+                     double last_zeta) {
     double zeta = last_zeta;
     for (uint64_t i = last_num + 1; i <= cur_num; ++i) {
       zeta += 1 / std::pow(i, theta);
     }
     return zeta;
   }
-  
+
   static double Zeta(uint64_t num, double theta) {
     return Zeta(0, num, theta, 0);
   }
-  
+
   uint64_t num_items_;
   uint64_t base_; /// Min number of items to generate
-  
+
   // Computed parameters for generating the distribution
   double theta_, zeta_n_, eta_, alpha_, zeta_2_;
   uint64_t n_for_zeta_; /// Number of items used to compute zeta_n
@@ -97,14 +98,14 @@ inline uint64_t ZipfianGenerator::Next(uint64_t num) {
     RaiseZeta(num);
     eta_ = Eta();
   }
-  
+
   double u = utils::RandomDouble();
   double uz = u * zeta_n_;
-  
+
   if (uz < 1.0) {
     return last_value_ = 0;
   }
-  
+
   if (uz < 1.0 + std::pow(0.5, theta_)) {
     return last_value_ = 1;
   }
@@ -117,6 +118,6 @@ inline uint64_t ZipfianGenerator::Last() {
   return last_value_;
 }
 
-}
+} // namespace ycsbc
 
 #endif // YCSB_C_ZIPFIAN_GENERATOR_H_
