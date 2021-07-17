24d49c65aeb6a6de4c454257b38bee6d7523982b
colin <colins110@hotmail.com>
2021-07-17 10:01:50 +0800

    add 100GB config file

--- bench/run_multi_perf.sh
+++ bench/run_multi_perf.sh
@@ -37,32 +37,36 @@ ssd="/mnt/sdb"
 
 
 
-
+workload="../workloads/workloada1KB100GB.spec"
 #load
 mod="multi-load-375M"
 echo "load start"
-sudo cgexec -g blkio:test_write ./ycsbc -db rocksdb load -dbPath /mnt/sdb/testRocksdb -threads 40 -P ../workloads/workloada1KB20GB.spec -dbConfig ../db_config/rocksdb_config.ini > load1KB20GB_${today}_${mod}.txt 2>&1
+benchName="load1KB100GB"
+sudo cgexec -g blkio:test_write ../build/ycsbc -db rocksdb load -dbPath /mnt/sdb/testRocksdb -threads 40 -P ${workload} -dbConfig ../db_config/rocksdb_config.ini > ${benchName}_${today}_${mod}.txt 2>&1
 
-sudo cp -r ${ssd}/testRocksdb ${ssd}/testRocksdbBackup
+sudo cp -r ${ssd}/testRocksdb ${ssd}/testRocksdbMultiBackup
 
 #workloada
 mod="multi-workloada-375M"
 echo "runA start"
-sudo cgexec -g blkio:test_write ./ycsbc -db rocksdb run -dbPath /mnt/sdb/testRocksdb -threads 40 -P ../workloads/workloada1KB20GB.spec -dbConfig ../db_config/rocksdb_config.ini > run1KB20GB_${today}_${mod}.txt 2>&1
+benchName="run1KB100GB"
+sudo cgexec -g blkio:test_write ../build/ycsbc -db rocksdb run -dbPath /mnt/sdb/testRocksdb -threads 40 -P ${workload} -dbConfig ../db_config/rocksdb_config.ini > ${benchName}_${today}_${mod}.txt 2>&1
 
 
 sudo rm -rf ${ssd}/testRocksdb
 sudo cp -r ${ssd}/testRocksdbBackup/* ${ssd}/testRocksdb
 #workloadb
 mod="multi-workloadb-375M"
+workload="../workloads/workloadb1KB100GB.spec"
 echo "runB start"
-sudo cgexec -g blkio:test_write ./ycsbc -db rocksdb run -dbPath /mnt/sdb/testRocksdb -threads 40 -P ../workloads/workloadb1KB20GB.spec -dbConfig ../db_config/rocksdb_config.ini > run1KB20GB_${today}_${mod}.txt 2>&1
+sudo cgexec -g blkio:test_write ../build/ycsbc -db rocksdb run -dbPath /mnt/sdb/testRocksdb -threads 40 -P ${workload} -dbConfig ../db_config/rocksdb_config.ini > ${benchName}_${today}_${mod}.txt 2>&1
 
 
 sudo rm -rf ${ssd}/testRocksdb
 sudo cp -r ${ssd}/testRocksdbBackup/* ${ssd}/testRocksdb
 #workloadd
 mod="multi-workloadd-375M"
+workload="../workloads/workloadd1KB100GB.spec"
 echo "runD start"
-sudo cgexec -g blkio:test_write ./ycsbc -db rocksdb run -dbPath /mnt/sdb/testRocksdb -threads 40 -P ../workloads/workloadd1KB20GB.spec -dbConfig ../db_config/rocksdb_config.ini > run1KB20GB_${today}_${mod}.txt 2>&1
+sudo cgexec -g blkio:test_write ../build/ycsbc -db rocksdb run -dbPath /mnt/sdb/testRocksdb -threads 40 -P ${workload} -dbConfig ../db_config/rocksdb_config.ini > ${benchName}_${today}_${mod}.txt 2>&1
 
---
+++ workloads/workloadd1KB100GB.spec
@@ -0,0 +1,22 @@
+# Yahoo! Cloud System Benchmark
+# Workload A: Update heavy workload
+#   Application example: Session store recording recent actions
+#                        
+#   Read/update ratio: 50/50
+#   Default data size: 1 KB records (10 fields, 100 bytes each, plus key)
+#   Request distribution: zipfian
+
+recordcount=104857600
+operationcount=20971520
+workload=com.yahoo.ycsb.workloads.CoreWorkload
+
+readallfields=true
+
+readproportion=0.95
+updateproportion=0
+scanproportion=0
+insertproportion=0.05
+
+fieldlength=1000
+requestdistribution=zipfian
+
